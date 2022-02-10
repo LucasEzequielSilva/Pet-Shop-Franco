a41dd3346d56c3a28a88edbbddf96853e8841f63
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-02-10 19:23:25 -0300

    Carga de cards dinamicas en pag. farmacia y juguetes

---
+++ scripts/juguetes.js
@@ -0,0 +1,39 @@
+let datosApi = []
+const cardContenedor = document.getElementById("cardContainer")
+
+async function getData() {
+    await fetch("https://apipetshop.herokuapp.com/api/articulos")
+        .then(response => response.json())
+        .then(dato => {
+            datosApi.push(...dato.response.filter(producto => producto.tipo.toLowerCase() == "juguete"))
+
+            imprimirProd()
+        })
+}
+
+getData()
+
+
+
+function imprimirProd() {
+
+    datosApi.forEach(producto => {
+        cardContenedor.innerHTML += (`
+        <div class="col">
+            <div class="card h-100 col-10 mx-auto">
+            <h5 class="card-title mx-auto text-center" style="width: 90%"> ${producto.nombre} </h5>
+                <img src="${producto.imagen}" style="objet-fit:cover;" class="card-img-top" alt="...">
+                <div class="card-body">
+                    
+                    <p class="card-text">Precio : $${producto.precio}</p>
+                    <p class="card-text">Stock : ${producto.stock}u.</p>
+                    
+                    <p class="card-text">${producto.descripcion}</p>
+                </div>
+            </div>
+        </div>`)
+    })
+}
+
+
+
